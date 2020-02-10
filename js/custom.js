54f5414f51dccbcc2b6d1d78c905f979713a2ebf
Chet Ram <56634108+chetramewb@users.noreply.github.com>
2020-02-10 17:42:23 +0530

    style desktop

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,12 @@
  
+
 $('.slide-full').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 10000,
+  nextArrow: '<button class="next-arrow"> <i class="fas fa-angle-right"></i> </button>', 
+  prevArrow: '<button class="back-arrow"> <i class="fas fa-angle-left"></i> </button>', 
   
   responsive: [
   
@@ -58,6 +61,38 @@ $('.slide-box').slick({
   
   });
   
+     
+$('.testmonials-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  nextArrow: '<button class="next-arrow"> <i class="fas fa-angle-right"></i> </button>', 
+  prevArrow: '<button class="back-arrow"> <i class="fas fa-angle-left"></i> </button>', 
+  responsive: [
   
+  {
+  breakpoint: 992,
+  settings: {
+  slidesToShow: 1,
+  slidesToScroll: 1
+  }
+  },
+  {
+  breakpoint: 525,
+  settings: {
+  slidesToShow: 1,
+  slidesToScroll: 1
+  }
+  }
+  
+  ]
+  
+  });
+  
+  
+
+  
+
 
   
